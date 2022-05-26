53776dddaf1e3fd5741a665c474a72ef5f9a3c24
Waluyo Ade Prasetio <3275090110970014>
2022-05-27 00:45:54 +0700

    update node

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-const translate = require("@vitalets/google-translate-api");
-router.get("/translate/:from/:to", async function (req, res) {
-  try {
-    const result = await translate(req.query.text, {
-      to: req.params.to,
-      from: req.params.from,
-      client: "gtx",
-    })
-      .then((resul) => {
-        res.json({ message: "success", data: resul.raw[1][0][0][5][0][4] });
-      })
-      .catch((err) => {
-        // console.log("err", err);
-      });
-  } catch (err) {
-    console.log(err);
-  }
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
